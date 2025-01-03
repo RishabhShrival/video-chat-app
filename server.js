bd4e4b9db6e9d39d931ace441947ad373d08cce1
RishabhShrival <rishabhshrival746@gmail.com>
2025-01-03 15:29:58 +0530

    first

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+let users = []; // List of connected users
+
+app.use(express.static("public")); // Serve the frontend files
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+  users.push(socket.id);
+
+  // Send updated user list to all clients
+  io.emit("user-list", users);
+
+  // Handle signaling data
+  socket.on("signal", (data) => {
+    const { to, signal } = data;
+    io.to(to).emit("signal", { from: socket.id, signal });
+  });
+
+  // Handle user disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    users = users.filter((id) => id !== socket.id);
+    io.emit("user-list", users); // Update user list
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
